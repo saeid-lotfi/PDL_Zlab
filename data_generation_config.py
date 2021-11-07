a4ff02b69ccbd16e40189cd81d4a5e6488eb4e91
Saeid Lotfi <mehdi.lotfipour91@gmail.com>
2021-11-07 14:36:25 +0000

    data generator modification

--- data_generation_config.py
+++ data_generation_config.py
@@ -1,7 +1,7 @@
-from os import makedirs
-
-
 _number_of_samples_ = 20
 _image_size_ = (640, 480)
-_generation_path_ = 'Tensorflow/workspace/images/generated_data'
 _source_path_ = 'data'
+_generation_path_ = 'Tensorflow/workspace/images/generated_data'
+_annotation_path_ = 'Tensorflow/workspace/annotations'
+
+_labels_ = [{'name':'digit', 'id':1}]
--- data_generator.py
+++ data_generator.py
@@ -8,17 +8,19 @@ import data_generation_config
 #getting parameters
 number_of_samples = data_generation_config._number_of_samples_
 image_size = data_generation_config._image_size_
-generation_path = os.path.join(os.getcwd(), data_generation_config._generation_path_)
-source_path = os.path.join(os.getcwd(), data_generation_config._source_path_)
+source_path = data_generation_config._source_path_
+generation_path = data_generation_config._generation_path_
+annotation_path = data_generation_config._annotation_path_
+annotation_file = os.path.join(annotation_path, 'label_map.pbtxt')
 raw_digit_path = os.path.join(source_path, 'raw_digits')
 raw_backgrounds_path = os.path.join(source_path, 'raw_backgrounds')
+labels = data_generation_config._labels_
+
 
 #make directory for generated images
 os.makedirs(generation_path, exist_ok= True)
-
 #making samples
 for sample in range(number_of_samples):
-
     #select random digit
     #with transformation
     random_digit = np.random.randint(10)
@@ -26,32 +28,37 @@ for sample in range(number_of_samples):
     selected_digit = Image.open(f'{raw_digit_path}/{random_digit}.jpg').convert('L')
     selected_digit = selected_digit.resize((digit_wh, digit_wh))
     selected_digit = selected_digit.rotate(np.random.randint(-20, 20), fillcolor= 'white')
-
     #locate bounding box
-    
     x_min = np.random.randint(int(image_size[0] * 0.05), int(image_size[0] * 0.7))
     y_min = np.random.randint(int(image_size[1] * 0.05), int(image_size[1] * 0.7))
     x_max = x_min + digit_wh
     y_max = y_min + digit_wh
-
     #make mask
     mask = Image.new(mode= 'L', size= image_size, color= 'white')
     mask = np.array(mask)
     mask[y_min:y_max, x_min:x_max] = np.array(selected_digit)
     mask = Image.fromarray(mask)
-
     #foreground & background
     fg_color = tuple(np.random.randint(256, size= 3))
     fg_img = Image.new(mode= 'RGB', size= image_size, color= fg_color)
     random_back = np.random.randint(1, 6)
     bg_img = Image.open(f'{raw_backgrounds_path}/{random_back}.jpg').resize(image_size)
-
     #composing & saving
     image_path = f'{generation_path}/{sample}.jpg'
     new_image = Image.composite(image1= bg_img, image2= fg_img, mask= mask)
     new_image.save(image_path, 'JPEG')
-
     #annotations
     writer = Writer(image_path, image_size[0], image_size[1])
     writer.addObject('digit', x_min, y_min, x_max, y_max)
     writer.save(f'{generation_path}/{sample}.xml')
+
+
+#make directory for annotations
+os.makedirs(annotation_path, exist_ok= True)
+with open(annotation_file, 'w') as f:
+    for label in labels:
+        f.write('item { \n')
+        f.write('\tname:\'{}\'\n'.format(label['name']))
+        f.write('\tid:{}\n'.format(label['id']))
+        f.write('}\n')
+

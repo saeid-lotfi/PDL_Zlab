c2234ec0522fb798d1ce3f6cdecd98c6788ce94d
Saeid Lotfi <mehdi.lotfipour91@gmail.com>
2021-11-08 15:55:56 +0000

    adding pyparsing 2.4.7 to requirements

--- 01-setup.py
+++ 01-setup.py
@@ -5,11 +5,10 @@ import os
 for path in paths.values():
     os.makedirs(path, exist_ok= True)
 
-# installing requirements
-os.system('pip install -r requirements.txt')
-
 # cloning and installing TFOD API
 os.system(f"git clone https://github.com/tensorflow/models {paths['APIMODEL_PATH']}") # clone github
 os.system('apt-get install protobuf-compiler') # install protobuf compiler
 os.system('cd Tensorflow/models/research && protoc object_detection/protos/*.proto --python_out=. && cp object_detection/packages/tf2/setup.py . && python -m pip install . ')
 
+# installing requirements
+os.system('pip install -r requirements.txt')

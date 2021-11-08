6d305ebf8e1a02c9e3f91a76bc7ef95988c57c5c
Saeid Lotfi <mehdi.lotfipour91@gmail.com>
2021-11-08 10:15:49 +0000

    setup modification

--- 01-setup.py
+++ 01-setup.py
@@ -1,31 +1,12 @@
 import os
 
-CUSTOM_MODEL_NAME = 'my_ssd_mobnet' 
-PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
-PRETRAINED_MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz'
-TF_RECORD_SCRIPT_NAME = 'generate_tfrecord.py'
-LABEL_MAP_NAME = 'label_map.pbtxt'
-
-paths = {
-    'WORKSPACE_PATH': os.path.join('Tensorflow', 'workspace'),
-    'APIMODEL_PATH': os.path.join('Tensorflow','models'),
-    'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace','annotations'),
-    'IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images'),
-    'MODEL_PATH': os.path.join('Tensorflow', 'workspace','models'),
-    'PRETRAINED_MODEL_PATH': os.path.join('Tensorflow', 'workspace','pre-trained-models'),
-    'PROTOC_PATH':os.path.join('Tensorflow','protoc')
- }
-'''
-files = {
-    'PIPELINE_CONFIG':os.path.join('Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
-    'TF_RECORD_SCRIPT': os.path.join(paths['SCRIPTS_PATH'], TF_RECORD_SCRIPT_NAME), 
-    'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
-}
-'''
 
 # making project structure
 for path in paths.values():
     os.makedirs(path, exist_ok= True)
+
+# installing requirements
+os.system('pip install -r requirements.txt')
 
 # cloning and installing TFOD API
 os.system(f"git clone https://github.com/tensorflow/models {paths['APIMODEL_PATH']}") # clone github

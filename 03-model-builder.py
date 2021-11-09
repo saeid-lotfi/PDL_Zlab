7479a2a416a0b75043afab7dd426d2194650363b
Saeid Lotfi <mehdi.lotfipour91@gmail.com>
2021-11-09 16:54:51 +0000

    model builder module finished

--- 03-model-builder.py
+++ 03-model-builder.py
@@ -14,7 +14,8 @@ CUSTOM_MODEL_NAME = 'my_ssd_mobnet'
 PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
 PRETRAINED_MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz'
 CHECKPOINT_PATH = os.path.join('Tensorflow', 'workspace', 'models', CUSTOM_MODEL_NAME) # path for our specific model
-pipeline_file = os.path.join(CHECKPOINT_PATH, 'pipeline.config')
+PIPELINE_FILE = os.path.join(CHECKPOINT_PATH, 'pipeline.config')
+LABEL_FILE = os.path.join(paths['ANNOTATION_PATH'], 'label_map.pbtxt')
 
 # get pretrained model
 pretrained_model = wget.download(PRETRAINED_MODEL_URL) # download pretrained model
@@ -24,15 +25,16 @@ tar = tarfile.open(f"{paths['PRETRAINED_MODEL_PATH']}/{pretrained_model}", "r:gz
 tar.extractall(paths['PRETRAINED_MODEL_PATH'])
 tar.close()
 
-shutil.copy()
 # creating a template from a pretrained model in our custom model directory
+os.makedirs(CHECKPOINT_PATH, exist_ok= True) # make our directory
 shutil.copy(
     src= os.path.join(paths['PRETRAINED_MODEL_PATH'], PRETRAINED_MODEL_NAME, 'pipeline.config'),
     dst= os.path.join(CHECKPOINT_PATH, 'pipeline.config'))
 
+
 # getting config and change to protobuf format
 pipeline_config = pipeline_pb2.TrainEvalPipelineConfig() # make blank template
-with tf.io.gfile.GFile(pipeline_file, "r") as f:
+with tf.io.gfile.GFile(PIPELINE_FILE, "r") as f:
     proto_str = f.read()
     text_format.Merge(proto_str, pipeline_config)
 
@@ -41,12 +43,12 @@ pipeline_config.model.ssd.num_classes = len(configs._labels_)
 pipeline_config.train_config.batch_size = 2
 pipeline_config.train_config.fine_tune_checkpoint = os.path.join(paths['PRETRAINED_MODEL_PATH'], PRETRAINED_MODEL_NAME, 'checkpoint', 'ckpt-0')
 pipeline_config.train_config.fine_tune_checkpoint_type = "detection"
-pipeline_config.train_input_reader.label_map_path= paths['LABELMAP']
+pipeline_config.train_input_reader.label_map_path= LABEL_FILE
 pipeline_config.train_input_reader.tf_record_input_reader.input_path[:] = [os.path.join(paths['ANNOTATION_PATH'], 'train.record')]
-pipeline_config.eval_input_reader[0].label_map_path = paths['LABELMAP']
+pipeline_config.eval_input_reader[0].label_map_path = LABEL_FILE
 pipeline_config.eval_input_reader[0].tf_record_input_reader.input_path[:] = [os.path.join(paths['ANNOTATION_PATH'], 'test.record')]
 
 # write back new config file
 config_text = text_format.MessageToString(pipeline_config)
-with tf.io.gfile.GFile(pipeline_file, "wb") as f:
+with tf.io.gfile.GFile(PIPELINE_FILE, "wb") as f:
     f.write(config_text)

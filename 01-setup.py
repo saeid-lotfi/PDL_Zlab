2217785ba7c80e5ad2a451d52fc8cb21c37a399c
Saeid Lotfi <mehdi.lotfipour91@gmail.com>
2021-11-11 08:56:41 +0000

    making setup use tensorflow2.2

--- 01-setup.py
+++ 01-setup.py
@@ -12,6 +12,7 @@ os.system('cd Tensorflow/models/research && protoc object_detection/protos/*.pro
 
 # installing requirements
 os.system('pip install -r requirements.txt')
+os.system('pip install -U --pre tensorflow=="2.2.0"')
 
 # Verify Installation
 VERIFICATION_SCRIPT = os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection', 'builders', 'model_builder_tf2_test.py')
